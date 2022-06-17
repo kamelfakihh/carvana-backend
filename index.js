a44e12f9dec4ddea2b2f521a4e6a7d18a2e19076
kamelfakihh <kamelfakihh@gmail.com>
2022-06-17 06:54:42 +0300

    added error handling

--- index.js
+++ index.js
@@ -11,14 +11,36 @@ const {
     DB_HOST
 } = process.env;
 
-async function startServer(){
+async function connectDB(){
 
     try {
+        
+        //  example uri : "mongodb://localhost:21017/carvana"
+        const CONNECTION_URI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+        await mongoose.connect(CONNECTION_URI);
+
+        const db = mongoose.connection;
+        db.on('error', error => {
+            console.log("database connection error: " + error);
+        })
+
+        console.log("connected to database");
+
+    } catch(error){
+        console.log("failed to connect to database", error);
+    }
+}
+
+async function startServer(){
+
+    try {   
+        
+        await connectDB();       
 
         // intialize express app
         const app = express();
         
-        app.use(express.json());                
+        app.use(express.json());         
 
         // initialize routes
         app.use('/cars', carsRouter);      
